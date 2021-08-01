84b084ea95785e6e9f3944fa7d6cbae257a6e9f7
SomyajyotiTalukdar <87467104+SomyajyotiTalukdar@users.noreply.github.com>
2021-08-01 09:51:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
